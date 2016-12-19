24cab27cc323aa6dee3e46444172b21d36bc45eb
sabandyopadh <sabandyopadh@cs.stonybrook.edu>
2016-12-18 20:23:20 -0500

    Final pipelined code

--- train_data.py
+++ train_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sys
 import string
 from sklearn.pipeline import Pipeline
+from sklearn.pipeline import FeatureUnion
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfTransformer
 import sklearn.datasets
@@ -23,12 +24,15 @@ from sklearn import metrics
 import _pickle as cPickle
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.base import TransformerMixin
+import math
 
 bw_list = []
 
 class BadWordTransformer(TransformerMixin):
 
     def transform(self, X, **transform_params):
+        X = X.astype('str').map(lambda x: x.split() if (x!='nan') else '')
+        
         return pd.DataFrame(X.map(lambda x: len(list(filter((lambda y: y in bw_list), x) ))))
         
 
@@ -45,42 +49,38 @@ inp_text = re.split('\n|,', inp_text)
 
 bw_list= [word.replace( u"\xa0",u"") for word in inp_text]
 
-dataframe_dataset["replaced stemmed"] = \
-        dataframe_dataset["replaced stemmed"].astype('str'). \
-        map(lambda x: x[1:-1].split(',') if (x!='nan') else '')
 
-dataframe_dataset["replaced stemmed"] = \
-        dataframe_dataset["replaced stemmed"]. \
-        map(lambda x: list(map(lambda s:str(s.strip("' ")), x)))
+dataframe_dataset["joined replaced stemmed"].fillna(" ", inplace=True)
 
-print(dataframe_dataset["replaced stemmed"])
+print(dataframe_dataset["joined replaced stemmed"])
 print("Using Naive Baye's")
+'''
 text_clf = Pipeline([('vect', CountVectorizer(ngram_range=(2, 2))),
                      ('tfidf', TfidfTransformer()),
                      ('clf', MultinomialNB()),
                      ])
 '''
-text_clf = Pipeline([#('vect', CountVectorizer(ngram_range=(1, 1))),
-                     ('tfidf', BadWordTransformer()),
-                     ('clf', MultinomialNB()),
-                     ])
-'''
-text_clf = text_clf.fit(dataframe_dataset["replaced stemmed"],dataframe_dataset["Insult"])
+text_clf = Pipeline([
+    ('features', FeatureUnion([
+        ('ngram_tf_idf', Pipeline([
+            ('counts', CountVectorizer(ngram_range=(2, 2))),
+            ('tf_idf', TfidfTransformer())
+            ])),
+        ('badwords', BadWordTransformer())
+        ])),
+    ('classifier', MultinomialNB())
+    ])
+
+text_clf = text_clf.fit(dataframe_dataset["joined replaced stemmed"],dataframe_dataset["Insult"])
 
 print("Print here the mean value")
 print("Print metric report")
 
-
 # Testing
 dataframe_dataset_test = pd.read_csv("./Test_clean.csv", na_values='unknown', encoding="utf-8")
-dataframe_dataset_test["replaced stemmed"] = \
-        dataframe_dataset_test["replaced stemmed"].astype('str'). \
-        map(lambda x: x[1:-1].split(',') if (x!='nan') else '')
-dataframe_dataset_test["replaced stemmed"] = \
-        dataframe_dataset_test["replaced stemmed"]. \
-        map(lambda x: list(map(lambda s:str(s.strip("' ")), x)))
+dataframe_dataset_test["joined replaced stemmed"].fillna(" ", inplace=True)
 
-predicted = text_clf.predict(dataframe_dataset_test["replaced stemmed"])
+predicted = text_clf.predict(dataframe_dataset_test["joined replaced stemmed"])
 print(np.mean(predicted == dataframe_dataset_test["Insult"]))
 print(metrics.classification_report(dataframe_dataset_test["Insult"], predicted, target_names=target_names))
 
