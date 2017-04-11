9dc8ac3e462bf8d44a4bc42b35bf95c577c534c1
sabandyopadh <sabandyopadh@cs.stonybrook.edu>
2017-04-10 22:56:48 -0400

    final commit with script to test model

--- Project_preprocess_data.py
+++ Project_preprocess_data.py
@@ -1,4 +1,4 @@
-# Code to preprocess data
+
 import sys, math, re
 from operator import itemgetter
 import math
@@ -20,40 +20,40 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 def clean_dataset(dataframe_dataset):
 
-    dataframe_dataset['Backup Comment'] = dataframe_dataset['Comment']
+# The list for the corresponding 1 and 0 values of the comments
+    dataframe_target = dataframe_dataset[['Insult']]
+    insult_target = dataframe_target.ix[:,0].tolist()
+
+# Removing \n
+    dataframe_dataset['Comment'] = dataframe_dataset['Comment'].str.replace('\n', '')
+
+# Removing all HTML Tags
+    dataframe_dataset['Comment'] = dataframe_dataset['Comment'].str.replace('[^\w\s]','')
+
 # Converting all uppercase letters to lower case
     dataframe_dataset['Comment'] = dataframe_dataset['Comment'].str.lower()
-# Removing \n
-    dataframe_dataset['Comment'] = dataframe_dataset['Comment'].replace(r'\n', '')
-    print(dataframe_dataset['Comment'])
-# Removing all HTML Tags
-    dataframe_dataset['Comment'] = dataframe_dataset['Comment'].str.replace('[^\w\s]',' ')
-    dataframe_dataset['Comment'] = dataframe_dataset['Comment'].str.replace('http\S+','')
-    dataframe_dataset['Comment'] = dataframe_dataset['Comment'].str.replace('www\S+','')
-
-# Removing all the non ASCII characters
-    dataframe_dataset['Comment'] = dataframe_dataset["Comment"].map(lambda x: ''.join([" " if ord(i) < 32 or ord(i) > 126 else i for i in x]))
-    dataframe_dataset['Comment'] = dataframe_dataset['Comment'].str.replace('xa0',' ')
-    dataframe_dataset['Comment'] = dataframe_dataset['Comment'].str.replace('xc2',' ')
-# Removing the _ with ' '
-    dataframe_dataset['Comment'] = dataframe_dataset['Comment'].str.replace('_',' ')
-    
-# Trimming all the white spaces
-    dataframe_dataset['Comment'] = pd.core.strings.str_strip(dataframe_dataset['Comment'])
 
 #Tokenizing all the words in a given sentence
-    dataframe_dataset['tokenized_sents'] = dataframe_dataset['Comment'].map(lambda row: nltk.word_tokenize(row))
-    
-# A custom stop words list which does not exclude words which have a referral meaning like you, we etc
-    cust_list = open("Stop_words_custom_list.txt", 'r')
-    cust_list = cust_list.read()
-    cust_stop_list = re.split('\n|,', cust_list)
-    #print(cust_stop_list)
-    dataframe_dataset['tokenized_sents'] = dataframe_dataset['tokenized_sents'].apply(lambda x: [item for item in x if item not in cust_stop_list])
+    dataframe_dataset['tokenized_sents'] = dataframe_dataset.apply(lambda row: nltk.word_tokenize(row['Comment']), axis=1)
+
+#Removing all the stop words which have no meaning
+    stop = stopwords.words('english')
+    dataframe_dataset['tokenized_sents'] = dataframe_dataset['tokenized_sents'].apply(lambda x: [item for item in x if item not in stop])
 
 # Stemming all the words
     stemmer = SnowballStemmer('english')
     dataframe_dataset['stemmed'] = dataframe_dataset["tokenized_sents"].apply(lambda x: [stemmer.stem(y) for y in x])
+
+# Separating the list of both insulting and non insulting comments
+    '''for i in dataframe_dataset['Insult']:
+        if dataframe_dataset.iloc[i]['Insult'] == 1:
+
+            insulting_list.append(dataframe_dataset['stemmed'])
+
+    print(insulting_list)'''
+    '''dataframe_dataset_filter = dataframe_dataset.loc[dataframe_dataset['Insult'] == 1]
+    insulting_list = dataframe_dataset_filter['stemmed'].tolist()
+    print(insulting_list)'''
 
 # Replacing shorthand and other internet slangs with correct phrases
     map_words = {"u": "you", "em":"them", "da":"the", "yo":"you",
@@ -64,11 +64,24 @@ def clean_dataset(dataframe_dataset):
 
     dataframe_dataset['replaced stemmed'] = dataframe_dataset["stemmed"].map(lambda x: [map_words[x[i]] if x[i] in map_words else x[i] for i in range(len(x)) ])
 
-    dataframe_dataset['joined replaced stemmed'] = \
-            dataframe_dataset["replaced stemmed"]. \
-            map(lambda x: (' '.join(word for word in x)))
-
     return
+
+
+
+
+inp = open(path, 'r')
+inp_header = inp.readline()
+header_list = re.split("[,\n]" , inp_header)
+
+
+# Taking google list of bad words
+bw = open('full-list-of-bad-words.txt', 'r')
+
+#inp_text = bw.read()  # raeding file generated from puzzleenerator.py
+#inp_text = re.split('\n|,', inp_text)
+#print(inp_text)
+
+#print dataframe_dataset.applymap(lambda x: isinstance(x, (int, float))).all(0)
 
 #Reading Train Dataset
 path = r'./train.csv'
@@ -78,10 +91,9 @@ clean_dataset(dataframe_dataset)
 dataframe_dataset.to_csv('Train_clean.csv')
 
 #Reading Test Dataset
-path = r'./test_with_solutions.csv'
+path = r'./Test/test_with_solutions.csv'
 print("Path to test dataset is \n" + path)
-dataframe_dataset_test = pd.read_csv(path, na_values='unknown', encoding="utf-8")
-clean_dataset(dataframe_dataset_test)
-dataframe_dataset_test.to_csv('Test_clean.csv')
-
+dataframe_dataset = pd.read_csv(path, na_values='unknown', encoding="utf-8")
+clean_dataset(dataframe_dataset)
+dataframe_dataset.to_csv('Train_clean.csv')
 
--- train_data.py
+++ train_data.py
@@ -1,241 +1,42 @@
 # Using sklearn features to train the data
-import pandas as pd
-import sys
-import string
-from sklearn.pipeline import Pipeline
-from sklearn.pipeline import FeatureUnion
-from sklearn.svm import LinearSVC
-from sklearn.feature_extraction.text import TfidfTransformer
-import sklearn.datasets
-import sklearn.metrics
-import sklearn.svm
-import sklearn.naive_bayes
-import sklearn.neighbors
-import os
-import nltk
-import re
-from sklearn.model_selection import learning_curve
-from sklearn.model_selection import ShuffleSplit
-from sklearn.feature_extraction.text import CountVectorizer
-from nltk.stem.porter import PorterStemmer
-from sklearn.datasets import load_files
-import numpy as np
-from sklearn.cluster import KMeans
-from sklearn import metrics
-import _pickle as cPickle
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import LinearSVC
-from sklearn.base import TransformerMixin
-import math
-from sklearn.neural_network import MLPClassifier
-from sklearn.neural_network import MLPRegressor
-import matplotlib.pyplot as plt
-
-bw_list = []
-
-class BadWordTransformer(TransformerMixin):
-
-    def transform(self, X, **transform_params):
-        X = X.astype('str').map(lambda x: x.split() if (x!='nan') else '')
-        
-        return pd.DataFrame(X.map(lambda x: len(list(filter((lambda y: y in bw_list), x) ))))
-        
-
-    def fit(self, X, y=None, **fit_params):
-        return self
-
-target_names = ['Not an insult', 'Insult']
-
 # Training
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+
+
+
 dataframe_dataset = pd.read_csv("./Train_clean.csv", na_values='unknown', encoding="utf-8")
-bw = open('bad_words.txt', 'r')
-inp_text = bw.read()  # raeding file generated from puzzleenerator.py
-inp_text = re.split('\n|,', inp_text)
 
-bw_list= [word.replace( u"\xa0",u"") for word in inp_text]
-
-
-dataframe_dataset["joined replaced stemmed"].fillna(" ", inplace=True)
-
-print(dataframe_dataset["joined replaced stemmed"])
 print("Using Naive Baye's")
-'''
 text_clf = Pipeline([('vect', CountVectorizer(ngram_range=(2, 2))),
                      ('tfidf', TfidfTransformer()),
                      ('clf', MultinomialNB()),
                      ])
-'''
+print(len(dataframe_dataset["replaced stemmed"]))
+text_clf = text_clf.fit(dataframe_dataset["replaced stemmed"])
 
-# Naive Baye's
-text_clf = Pipeline([
-    ('features', FeatureUnion([
-        ('ngram_tf_idf', Pipeline([
-            ('counts', CountVectorizer(ngram_range=(2, 2))),
-            ('tf_idf', TfidfTransformer())
-            ])),
-        ('badwords', BadWordTransformer())
-        ])),
-    ('classifier', MultinomialNB())
-    ])
-
-text_clf = text_clf.fit(dataframe_dataset["joined replaced stemmed"],dataframe_dataset["Insult"])
-
-print("Print here the mean value of Naive Baye's")
-print("Print metric report of Naive Baye's")
-
-# Testing
-dataframe_dataset_test = pd.read_csv("./Test_clean.csv", na_values='unknown', encoding="utf-8")
-dataframe_dataset_test["joined replaced stemmed"].fillna(" ", inplace=True)
-
-predicted = text_clf.predict(dataframe_dataset_test["joined replaced stemmed"])
-print(np.mean(predicted == dataframe_dataset_test["Insult"]))
-print(metrics.classification_report(dataframe_dataset_test["Insult"], predicted, target_names=target_names))
-naive_bayes = []
-for n in range(100, len(dataframe_dataset), 400):
-    text_clf = text_clf.fit(dataframe_dataset["joined replaced stemmed"].head(n),dataframe_dataset["Insult"].head(n))
-    predicted = text_clf.predict(dataframe_dataset_test["joined replaced stemmed"])
-    naive_bayes.append(metrics.f1_score(dataframe_dataset_test["Insult"], predicted, average='weighted'))
+print("Print here the mean value")
+print("Print metric report")
+docs_test = twenty_test.data
 
 
-# LinearSVC
-text_clf = Pipeline([
-    ('features', FeatureUnion([
-        ('ngram_tf_idf', Pipeline([
-            ('counts', CountVectorizer(ngram_range=(2, 2))),
-            ('tf_idf', TfidfTransformer())
-            ])),
-        ('badwords', BadWordTransformer())
-        ])),
-    ('classifier', LinearSVC())
-    ])
 
-text_clf = text_clf.fit(dataframe_dataset["joined replaced stemmed"],dataframe_dataset["Insult"])
+predicted = text_clf.predict(docs_test)
+print(np.mean(predicted == twenty_test.target))
 
-print("Print here the mean value of LinearSVC")
-print("Print metric report of LinearSVC")
+print(metrics.classification_report(twenty_test.target, predicted, target_names=twenty_test.target_names))
 
-# Testing
-dataframe_dataset_test = pd.read_csv("./Test_clean.csv", na_values='unknown', encoding="utf-8")
-dataframe_dataset_test["joined replaced stemmed"].fillna(" ", inplace=True)
-
-predicted = text_clf.predict(dataframe_dataset_test["joined replaced stemmed"])
-print(np.mean(predicted == dataframe_dataset_test["Insult"]))
-print(metrics.classification_report(dataframe_dataset_test["Insult"], predicted, target_names=target_names))
-linear_svc = []
-for n in range(100, len(dataframe_dataset), 400):
-    text_clf = text_clf.fit(dataframe_dataset["joined replaced stemmed"].head(n),dataframe_dataset["Insult"].head(n))
-    predicted = text_clf.predict(dataframe_dataset_test["joined replaced stemmed"])
-    linear_svc.append(metrics.f1_score(dataframe_dataset_test["Insult"], predicted, average='weighted'))
-
-
-#Logistic Regresson
-text_clf = Pipeline([
-    ('features', FeatureUnion([
-        ('ngram_tf_idf', Pipeline([
-            ('counts', CountVectorizer(ngram_range=(2, 2))),
-            ('tf_idf', TfidfTransformer())
-            ])),
-        ('badwords', BadWordTransformer())
-        ])),
-    ('classifier', LogisticRegression())
-    ])
-
-text_clf = text_clf.fit(dataframe_dataset["joined replaced stemmed"],dataframe_dataset["Insult"])
-
-print("Print here the mean value of Logistic Regression")
-print("Print metric report of Logistic Regression")
-
-# Testing
-dataframe_dataset_test = pd.read_csv("./Test_clean.csv", na_values='unknown', encoding="utf-8")
-dataframe_dataset_test["joined replaced stemmed"].fillna(" ", inplace=True)
-
-predicted = text_clf.predict(dataframe_dataset_test["joined replaced stemmed"])
-print(np.mean(predicted == dataframe_dataset_test["Insult"]))
-print(metrics.classification_report(dataframe_dataset_test["Insult"], predicted, target_names=target_names))
-log_reg = []
-for n in range(100, len(dataframe_dataset), 400):
-    text_clf = text_clf.fit(dataframe_dataset["joined replaced stemmed"].head(n),dataframe_dataset["Insult"].head(n))
-    predicted = text_clf.predict(dataframe_dataset_test["joined replaced stemmed"])
-    log_reg.append(metrics.f1_score(dataframe_dataset_test["Insult"], predicted, average='weighted'))
-
-#Random Forest Classifier
-text_clf = Pipeline([
-    ('features', FeatureUnion([
-        ('ngram_tf_idf', Pipeline([
-            ('counts', CountVectorizer(ngram_range=(2, 2))),
-            ('tf_idf', TfidfTransformer())
-            ])),
-        ('badwords', BadWordTransformer())
-        ])),
-    ('classifier', RandomForestClassifier())
-    ])
-
-text_clf = text_clf.fit(dataframe_dataset["joined replaced stemmed"],dataframe_dataset["Insult"])
-
-print("Print here the mean value of Random Forest Classifier")
-print("Print metric report of Random Forest Classifier")
-
-# Testing
-dataframe_dataset_test = pd.read_csv("./Test_clean.csv", na_values='unknown', encoding="utf-8")
-dataframe_dataset_test["joined replaced stemmed"].fillna(" ", inplace=True)
-
-predicted = text_clf.predict(dataframe_dataset_test["joined replaced stemmed"])
-print(np.mean(predicted == dataframe_dataset_test["Insult"]))
-print(metrics.classification_report(dataframe_dataset_test["Insult"], predicted, target_names=target_names))
-
-random_forest = []
-for n in range(100, len(dataframe_dataset), 400):
-    text_clf = text_clf.fit(dataframe_dataset["joined replaced stemmed"].head(n),dataframe_dataset["Insult"].head(n))
-    predicted = text_clf.predict(dataframe_dataset_test["joined replaced stemmed"])
-    random_forest.append(metrics.f1_score(dataframe_dataset_test["Insult"], predicted, average='weighted'))
- 
- 
-# My_Best_Configuration
-text_clf = Pipeline([
-    ('features', FeatureUnion([
-        ('ngram_tf_idf', Pipeline([
-            ('counts', CountVectorizer(ngram_range=(1, 2))),
-            ('tf_idf', TfidfTransformer())
-            ])),
-        ('badwords', BadWordTransformer())
-        ])),
-    ('classifier', LinearSVC(penalty='l2', loss='squared_hinge',  C=1.0, fit_intercept=True, intercept_scaling=1, max_iter=1000))
-    ])
-
-text_clf = text_clf.fit(dataframe_dataset["joined replaced stemmed"],dataframe_dataset["Insult"])
-
-print("Print here the mean value of Best Configuration")
-print("Print metric report of Best Configuration")
-
-# Testing
-dataframe_dataset_test = pd.read_csv("./Test_clean.csv", na_values='unknown', encoding="utf-8")
-dataframe_dataset_test["joined replaced stemmed"].fillna(" ", inplace=True)
-
-predicted = text_clf.predict(dataframe_dataset_test["joined replaced stemmed"])
-print(np.mean(predicted == dataframe_dataset_test["Insult"]))
-print(metrics.classification_report(dataframe_dataset_test["Insult"], predicted, target_names=target_names))
-
-my_best_conf = []
-for n in range(100, len(dataframe_dataset), 400):
-    text_clf = text_clf.fit(dataframe_dataset["joined replaced stemmed"].head(n),dataframe_dataset["Insult"].head(n))
-    predicted = text_clf.predict(dataframe_dataset_test["joined replaced stemmed"])
-    my_best_conf.append(metrics.f1_score(dataframe_dataset_test["Insult"], predicted, average='weighted'))
-
-xaxis = [n for n in range(100, len(dataframe_dataset),400)]
-plt.plot(xaxis,naive_bayes,'r-', label='naive bayes')
-plt.plot(xaxis,linear_svc,'b-', label='linear SVC')
-plt.plot(xaxis,log_reg,'m-', label='Logistic Regression')
-plt.plot(xaxis,random_forest,'g-', label='random forest')
-plt.plot(xaxis,my_best_conf,'c-', label='Best Configuration')
-plt.xlabel('Training samples')
-plt.ylabel('F1 scores')
-plt.legend(prop={'size':10})
-plt.savefig('trainplot-learning.png')
-plt.show()
 # save the classifier
 with open('my_classifier.pkl', 'wb') as fid:
         cPickle.dump(text_clf, fid)
 
+SVC_values = []
+
+
+for n in range(100, train_data_length, 200):
+    text_clf = text_clf.fit(twenty_train.data[:n], twenty_train.target[:n])
+    predicted = text_clf.predict(docs_test)
+    SVC_values.append(metrics.f1_score(twenty_test.target, predicted, average='weighted'))
 
